ffc3bf2a96f217c259e8db82f6191d3987f8ee33
aurelien-senn <aurelien.senn.web@gmail.com>
2022-07-15 17:34:53 +0200

    add option color

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,37 +1,40 @@
-// recueration id dans url
-var urlProduct= document.location.href;
+// recuperation id dans url
+var urlProduct = document.location.href;
 var url = new URL(urlProduct);
-var idUrl= url.searchParams.get("id");
+var idUrl = url.searchParams.get("id");
 
-function afficherImageProduct(){
-    
-}
 
-fetch("http://localhost:3000/api/products/"+idUrl)
+//recupere array d un produit
+fetch("http://localhost:3000/api/products/" + idUrl)
     .then(function (data) {
         if (data.ok) {
             return data.json();
         }
     })
 
+    //insere le produit dans la page
+    .then(function (productList) {
+        //ajout balise <img>
+        console.log(productList);
+        let imageProduct = document.createElement('img');
+        const classProductImgHtml = document.querySelector('.item__img');
+        classProductImgHtml.prepend(imageProduct);
+        //ajout des attribut src et alt
+        const baliseImage = classProductImgHtml.childNodes[0];
+        baliseImage.setAttribute("src", productList.imageUrl);
+        baliseImage.setAttribute("alt", productList.altTxt);
+        //option+color
+        const classProductcolorsHtml = document.querySelector('#colors');
+        let i = 1
+        for (productList.colors of productList.colors) {
+            //ajout des balise options
+            let colorsProduct = document.createElement('option');
+            classProductcolorsHtml.append(colorsProduct);
+            // ajout des options couleurs
+            let baliseOption = document.getElementsByTagName('option');
+            baliseOption[i].setAttribute("value", productList.colors);
+            console.log(productList.colors);
+            i++;
+        }
 
-  .then(function (productList) {
-    //ajout balise <img>
-    let imageProduct = document.createElement('img');
-    const classProductHtml = document.querySelector('.item__img');
-    classProductHtml.prepend(imageProduct);
-    //ajout des attribut src et alt
-    const baliseImage = classProductHtml.childNodes[0];
-    baliseImage.setAttribute("src",productList.imageUrl);
-    baliseImage.setAttribute("alt",productList.altTxt);
-    // text title price description
-    let nodeTitle =document.querySelector('title');
-    console.log(nodeTitle);
-    console.log(productList.name);
-    nodeTitle.appendChild(productList.name);
-    document.querySelector('description').appendChild(productList.description);
-    document.querySelector('price').appendChild(productList.price);
-    
-    }
-            )
-    
+    })
