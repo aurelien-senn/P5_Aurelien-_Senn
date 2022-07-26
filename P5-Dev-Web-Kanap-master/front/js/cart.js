055bef39a42708334f635c57a9feeff22d420daa
aurelien-senn <aurelien.senn.web@gmail.com>
2022-07-26 17:11:14 +0200

    add validate form

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 let listLocalStorage = JSON.parse(localStorage.getItem('objetL'));
 console.log(listLocalStorage);
 lengthlistLocalStorage = listLocalStorage.length;
@@ -11,7 +7,6 @@ let total = 0;
 let totalQuantity = 0;
 
 for (let i in listLocalStorage) {
-
 
   async function fetchMoviesJSON() {
     const response = await fetch("http://localhost:3000/api/products/" + listLocalStorage[i].idProduct);
@@ -28,7 +23,7 @@ for (let i in listLocalStorage) {
     var divCartItemContentDescription = document.createElement('div');
     var h2 = document.createElement('h2');
     var pcolors = document.createElement('p');
-    var pprice = document.createElement('p');
+    var pPrice = document.createElement('p');
     var divCartItemContentSettings = document.createElement('div');
     var divCartItemContentSettingsQuantity = document.createElement('div');
     var inputQuantity = document.createElement('input');
@@ -47,7 +42,6 @@ for (let i in listLocalStorage) {
     divCartItemContentSettingsQuantity.setAttribute('class', 'cart__item__content__settings__quantity');
     divCartItemContentSettingsDelete.setAttribute('class', 'cart__item__content__settings__delete');
     // add attribut img
-
     img.setAttribute('src', product.imageUrl);
     img.setAttribute('alt', product.altText);
     // add attribut input
@@ -60,28 +54,20 @@ for (let i in listLocalStorage) {
     //Add attribut pDeleteItem
     pDeleteItem.setAttribute('class', 'deleteItem');
     // sel parent et ajout balise
-
-
     var nodeArticle = document.getElementById("cart__items");
-
     nodeArticle.appendChild(article);
-
     var nodeDivCartItem = document.getElementsByClassName("cart__item")[0];
     nodeDivCartItem.appendChild(divCartItemImg);
-
     article.appendChild(divCartItemImg);
     article.appendChild(divCartItemContent);
     divCartItemImg.appendChild(img);
     divCartItemContent.appendChild(divCartItemContentDescription);
     divCartItemContent.appendChild(divCartItemContentSettings);
-
     divCartItemContentDescription.appendChild(h2);
     divCartItemContentDescription.appendChild(pcolors);
-    divCartItemContentDescription.appendChild(pprice);
-
+    divCartItemContentDescription.appendChild(pPrice);
     divCartItemContent.appendChild(divCartItemContentSettingsDelete);
     divCartItemContentSettingsDelete.appendChild(pDeleteItem);
-
     divCartItemContentSettings.appendChild(divCartItemContentSettingsQuantity);
     divCartItemContentSettingsQuantity.appendChild(pQuantity);
     divCartItemContentSettingsQuantity.appendChild(inputQuantity);
@@ -90,38 +76,99 @@ for (let i in listLocalStorage) {
     pcolors.textContent = listLocalStorage[i].colorItems;
     //calcul total par article
     var pricebyvanap = Number(listLocalStorage[i].quantityItem) * product.price;
-    pprice.textContent = pricebyvanap + " €";
+    pPrice.textContent = pricebyvanap + " €";
     pQuantity.textContent = "Qté : ";
     pDeleteItem.textContent = "Supprimer";
     // calcul total et ajout
     total = total + pricebyvanap;
     totalQuantity = totalQuantity + Number(listLocalStorage[i].quantityItem);
-
-    var nodeTotal = document.getElementById("totalPrice");
-    nodeTotal.textContent = total;
-    var nodeTotal = document.getElementById("totalQuantity");
-    nodeTotal.textContent = totalQuantity;
-
+    var nodeTotalPrice = document.getElementById("totalPrice");
+    nodeTotalPrice.textContent = total;
+    var nodeTotalquantity = document.getElementById("totalQuantity");
+    nodeTotalquantity.textContent = totalQuantity;
     //suppression product cart
     pDeleteItem.addEventListener('click', () => {
       listLocalStorage.splice(i, 1);
       localStorage.setItem("objetL", JSON.stringify(listLocalStorage));
       document.location.reload(true);
-
-    
-
-      })
-
     })
+    //modifictaion quantity input
+    inputQuantity.onchange = function () {
+      var differencyQuantity = this.value - listLocalStorage[i].quantityItem;
+      //change quantity in input
+      listLocalStorage[i].quantityItem = this.value;
+      localStorage.setItem("objetL", JSON.stringify(listLocalStorage));
+      inputQuantity.setAttribute('value', listLocalStorage[i].quantityItem);
+      //change price
+      var oldPrice = pricebyvanap;
+      pricebyvanap = Number(listLocalStorage[i].quantityItem) * product.price;
+      pPrice.textContent = pricebyvanap + " €";
+      //change quantity
+      totalQuantity = totalQuantity + differencyQuantity;
+      nodeTotalquantity.textContent = totalQuantity;
+      //change total price
+      total = total + pricebyvanap - oldPrice;
+      nodeTotalPrice.textContent = total;
+    }
+  })
 
 
+}
 
+const contact = document.getElementById('order');
+contact.addEventListener('click', function () {
+  event.preventDefault();
 
-
+  var contactTab = {
+    firstName: document.getElementById('firstName').value,
+    lastName: document.getElementById('lastName').value,
+    address: document.getElementById('address').value,
+    city: document.getElementById('city').value,
+    email: document.getElementById('email').value,
+  };
+  var bolFirstName=(/^[a-zA-Z\-]+$/.test(contactTab.firstName));
+  var bollastName=/^[a-zA-Z\-]+$/.test(contactTab.lastName);
+  var bolemail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(contactTab.email);
+  var boladdress=/^[a-zA-Z0-9\s,.'-]{3,}$/.test(contactTab.address);
+  var bolcity=/^[a-zA-Z0-9\u00C0-\u00FF\s,. '-]{3,}$/.test(contactTab.city);
+  // validate firstName
+  if(bolFirstName)  {
+    firstNameErrorMsg.innerHTML = "";
+  } else {
+    firstNameErrorMsg.textContent = "ceci n\'est pas un prénom";
+    event.preventDefault();
   }
-
-
-
-
-
+  //validate email
+  if (bolemail) {
+    emailErrorMsg.innerHTML = "";
+  } else {
+    emailErrorMsg.textContent = "ceci n\'est pas un email";
+    event.preventDefault();
+  }
+  //validate lastName
+  if (bollastName) {
+    lastNameErrorMsg.innerHTML = "";
+  } else {
+    lastNameErrorMsg.textContent = "ceci n\'est pas un nom";
+    event.preventDefault();
+  }
+  // validate adress
+  if (boladdress) {
+    addressErrorMsg.innerHTML = "";
+  } else {
+    addressErrorMsg.textContent = "ceci n'est pas une adresse";
+    event.preventDefault();
+  }
+  //validate city
+  if (bolcity) {
+    cityErrorMsg.innerHTML = "";
+  } else {
+    cityErrorMsg.textContent = "ceci n'est pas une ville";
+    event.preventDefault()
+  }
+  // post contact et product
+  if (bolFirstName && bollastName && bolemail && boladdress && bolcity){
+    console.log('ok');
+  }
+})
 
